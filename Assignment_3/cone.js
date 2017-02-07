03519891a1526302ed76bb4495edfd33d23e5b12
DoctorNoname98 <dr.noname98@gmail.com>
2017-02-07 15:44:53 -0800

    Update cone.js

--- Assignment_3/cone.js
+++ Assignment_3/cone.js
@@ -2,7 +2,7 @@ var gl = null;
 var cone = null;
 
 function init() {
-    cone = new Cone(4, "Cone-vertex-shader", "Cone-fragment-shader");
+    cone = new Cone(4);
     var canvas = document.getElementById( "webgl-canvas" );
 
     gl = WebGLUtils.setupWebGL( canvas );
