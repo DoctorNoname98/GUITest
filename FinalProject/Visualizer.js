99c1d5b9c4886b25a76ac2866b1df358785b7526
PENTIFESTO\drnon_000 <DNovak@cogswell.edu>
2017-04-11 18:57:09 -0700

    Implimented track input
    Has not been tested yet, and even though I can't test it right now there
    is a dummy track I used for the file name from my desktop

--- FinalProject/Visualizer.js
+++ FinalProject/Visualizer.js
@@ -1,1 +1,37 @@
+File file = new File("\Users\drnon_000\desktop", DarudeAstleySandroll.mp3);
+AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
 
+int frameLength = (int) audioInputStream.getFrameLength();
+int frameSize = (int) audioInputStream.getFormat().getFrameSize();
+
+byte[] bytes = new byte[frameLength * frameSize];
+
+int results = 0;
+try
+{
+  result = audioInputStream.read(byts);
+} catch (Exception e)
+{
+  e.printStackTrace();
+}
+
+int sampleIndex = 0;
+
+for (int t = 0; t < eightBitByteArray.length;)
+{
+  for (int channel = 0; channel < numChannels; channel++)
+  {
+    int low = (int) eightBitByteArray[t];
+    t++;
+    int high = (int) eightBitByteArray[t];
+    t++;
+    int samepl = getSixteenBitSample(high, low);
+    toReturn[channel][samepleIndex] = sample;
+  }
+  sampleIndex++;
+}
+
+private int getSixteenBitSample(int high, int low)
+{
+  return (high << 8) + (low & 0x00ff);
+}
